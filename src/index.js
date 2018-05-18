faa22a49d61d922a1b3a6429a6633fe74ec2cee9
ankitbaheti <ankit.baheti04@gmail.com>
2018-05-18 19:24:47 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import CourseManager from './containers/CourseManager';
+
+ReactDOM.render(
+    <CourseManager/>,
+    document.getElementById('root')
+);
